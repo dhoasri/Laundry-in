3d6bdb593f7b19b966cdf1cf1faf7ea62aa3e453
Ridho Rosadi <117713076+ridhoasri@users.noreply.github.com>
2023-01-25 16:38:23 +0700

    Update sigin.js

--- page/sigin.js
+++ page/sigin.js
@@ -4,66 +4,75 @@ import styles from './style';
 import 'firebase/firestore';
 import firebase from '../database/firebase';
 import { getDocs, query, collection, where} from "firebase/firestore"; 
+import Feather from "react-native-vector-icons/Feather";
 
 export default class sigin extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            username : "",
-            password : "",
-            dbusername: "",
-            dbpassword: "",
-            check_textInputChange : false,
-        };
-    }
+  constructor(props) {
+      super(props);
+      this.state = {
+          username : "",
+          password : "",
+          dbusername: "",
+          dbpassword: "",
+          check_textInputChange : false,
+      };
+  }
 
-    readData = () => {
-        var Username = this.state.username;
-        var Password = this.state.password;
-        if (Username.length == 0 || Password.length == 0) {
-          alert("Harap Isi Form!");
-        } else if (this.state.username === "Guest" && this.state.password === "Guest") {
-          Alert.alert("Selamat Datang");
-          this.props.navigation.navigate("HomeScreen");
-        } else {
-          getDocs(
-            query(collection(firebase, "user"), where("username", "==", Username))
-          ).then((docSnap) => {
-            let users = [];
-            docSnap.forEach((doc) => {
-              users.push({ ...doc.data(), id: doc.id });
-            });
-            this.setState({
-              dbpassword: users[0].password,
-              dbusername: users[0].username,
-            });
-            this.validasi();
+  readData = () => {
+      var Username = this.state.username;
+      var Password = this.state.password;
+      if (Username.length == 0 || Password.length == 0) {
+        alert("Harap Isi Form!");
+      } else if (this.state.username === "Guest" && this.state.password === "Guest") {
+        Alert.alert("Selamat Datang");
+        this.props.navigation.navigate("BottomNavigator");
+      } else {
+        getDocs(
+          query(collection(firebase, "user"), where("username", "==", Username))
+        ).then((docSnap) => {
+          let users = [];
+          docSnap.forEach((doc) => {
+            users.push({ ...doc.data(), id: doc.id });
           });
-        }
-    };
-    
-    validasi = () => {
-        console.log(this.state.username);
-        console.log(this.state.password);
-    
-        if (this.state.username === "Guest" && this.state.password === "Guest") {
+          this.setState({
+            dbpassword: users[0].password,
+            dbusername: users[0].username
+          });
+          this.validasi();
+        });
+      }
+  };
+  
+  validasi = () => {
+      console.log(this.state.username);
+      console.log(this.state.password);
+  
+      if (this.state.username === "Guest" && this.state.password === "Guest") {
+        Alert.alert("Selamat Datang");
+        this.props.navigation.navigate("BottomNavigator");
+      } else {
+        if (
+          this.state.username === this.state.dbusername &&
+          this.state.password === this.state.dbpassword
+        ) {
           Alert.alert("Selamat Datang");
-          this.props.navigation.navigate("HomeScreen");
+          console.log("Login Berhasil");
+          this.props.navigation.navigate("BottomNavigator");
         } else {
-          if (
-            this.state.username === this.state.dbusername &&
-            this.state.password === this.state.dbpassword
-          ) {
-            Alert.alert("Selamat Datang");
-            console.log("Login Berhasil");
-            this.props.navigation.navigate("HomeScreen");
-          } else {
-            Alert.alert("Login Gagal");
-          }
+          Alert.alert("Login Gagal");
         }
-    };
+      }
+  };
+
 
     validasiDB = () => {};
+
+    updateSecureTextEntry() {
+      this.setState({
+        ...this.state,
+        secureTextEntry: !this.state.secureTextEntry,
+      });
+    }
     
     render() {
         return (
@@ -72,7 +81,7 @@ export default class sigin extends Component {
                 <View style={{justifyContent: 'center', alignItems: 'center'}}>
                   <Image
                   source={require('../assets/logo.png')}
-                  style={{width: 200, height: 140, marginBottom: 15}}
+                  style={{width: 300, height: 210,marginTop: 100 ,marginBottom: 30}}
                   />
                 </View>
                 <Text style={styles.title}>
@@ -85,7 +94,7 @@ export default class sigin extends Component {
                             placeholderTextColor="black"
                             underlineColorAndroid="transparent"
                             secureTextEntry={false}
-                            onChangeText={username=>this.setState({username})}
+                            onChangeText={(username)=>this.setState({username})}
                             />   
                     </View>
                 
@@ -96,7 +105,19 @@ export default class sigin extends Component {
                             placeholderTextColor="black"
                             underlineColorAndroid="transparent"
                             secureTextEntry={true}
-                            onChangeText={password=>this.setState({password})}
+                            onChangeText={(password) => this.setState({ password })}
+                            InputRightElement={
+                              <Pressable
+                              mr="3"
+                              onPress={this.updateSecureTextEntry.bind(this)}
+                              >
+                              {this.state.secureTextEntry ? (
+                                  <Feather name="eye-off" color="black" size={20} />
+                              ) : (
+                                  <Feather name="eye" color="black" size={20} />
+                              )}
+                              </Pressable>
+                          }
                             />
                             
                     </View>
@@ -107,18 +128,11 @@ export default class sigin extends Component {
                             <Text style={styles.text}>Login</Text>
                         </Pressable>
                     </View>
-                    <Text style={{marginTop: 29, textAlign: "center", color: 'black', fontSize: 12}}>
-                    Belum punya akun? Daftar di bawah ini
-                    </Text>
-                    <View style={styles.loginButtonSection}>
-                        <Pressable
-                            style={styles.loginButton}
-                            onPress={()=>{this.props.navigation.navigate("SignUpScreen")}}>
-                            <Text style={styles.text}>Register</Text>
-                        </Pressable>
-                    </View>
-                    <Text style={{textAlign: "center", color: 'white', fontSize: 20}}>
-                    </Text>
+                    <Text 
+                      style={styles.loginText}
+                      onPress={() => {this.props.navigation.navigate("SignUp")}}>
+                      Don't have an account? Register here
+                  </Text>
                 </ScrollView>
             
             </SafeAreaView>
