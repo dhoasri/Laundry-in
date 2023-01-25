a3711edb7d73bf4207d02c930d0dcf436691b275
Ridho Rosadi <117713076+ridhoasri@users.noreply.github.com>
2023-01-25 16:37:24 +0700

    Update App.js

--- App.js
+++ App.js
@@ -1,6 +1,11 @@
+import React from 'react';
+import { NativeBaseProvider, Icon, Text } from 'native-base';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from "react-navigation-stack";
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createAppContainer, createSwitchNavigator } from "react-navigation";
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 import SignIn from "./page/sigin.js";
 import SignUp from "./page/signup.js";
 import front from "./page/front.js";
@@ -9,42 +14,109 @@ import Maps from "./page/maps.js";
 import RiwayatLokasi from "./page/riwayatlokasi.js";
 import Profil from "./page/profil.js";
 
-const screens = {
-  SignInScreen: {
-    screen: SignIn,
-  },
-  SignUpScreen: {
-    screen: SignUp,
-  },
-  HomeScreen: {
-    screen: Home,
-  },
-  MapsScreen: {
-    screen: Maps,
-  },
-  RiwayatLokasiScreen: {
-    screen: RiwayatLokasi,
-  },
-  ProfilScreen: {
-    screen: Profil,
-  },
+const Tab = createBottomTabNavigator();
+const Stack = createNativeStackNavigator();
+
+const BottomNavigator = () => {
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+          if (route.name === 'Home') {
+            iconName = 'home';
+          } else if (route.name === 'Maps') {
+            iconName = 'google-maps';
+          } else if (route.name === 'Profil') {
+            iconName = 'account';
+          } 
+          return (
+            <Icon
+              as={MaterialCommunityIcons}
+              name={iconName}
+              size={30}
+              color={focused ? 'primary.600' : '#ffff'}
+            />
+          );
+        },
+        tabBarIconStyle: { marginTop: 10 },
+        tabBarLabel: ({ children, color, focused }) => {
+          return (
+            <Text color={focused ? 'primary.600' : color} mb={2}>
+              {children}
+            </Text>
+          );
+        },
+        tabBarStyle: {
+          height: 60,
+          borderTopWidth: 0,
+          backgroundColor: '#2396f2',
+          borderTopLeftRadius: 20,
+          borderTopRightRadius: 20
+        },
+      })}>
+      <Tab.Screen
+        name="Home"
+        component={Home}
+        options={{
+          unmountOnBlur: true,
+          headerShown: false,
+          tabBarLabel: () => {
+            return null;
+          },
+        }}
+      />
+      <Tab.Screen
+        name="Maps"
+        component={Maps}
+        options={{
+          unmountOnBlur: true,
+          headerShown: false,
+          tabBarLabel: () => {
+            return null;
+          },
+        }}
+      />
+      <Tab.Screen
+        name="Profil"
+        component={Profil}
+        options={{
+          unmountOnBlur: true,
+          headerShown: false,
+          tabBarLabel: () => {
+            return null;
+          },
+        }}
+      />
+    </Tab.Navigator>
+  );
 };
 
-const App = createStackNavigator(
-  screens,
-  {
-    defaultNavigationOptions: {
-      headerStyle: {
-        backgroundColor: "#2396f2",
-      },
-      headerTintColor: '#fff',
-      headerTitleStyle: {
-        fontWeight: 'bold',
-      },
-    },
-  },
-  {initialRouteName: 'SignInScreen'}
-);
+const App = () => {
+  return (
+    <NativeBaseProvider>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="SignIn"
+            component={SignIn}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="BottomNavigator"
+            component={BottomNavigator}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="SignUp"
+            component={SignUp}
+            options={{ headerShown: false }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </NativeBaseProvider>
+  );
+};
 
 const RootNavigator = createSwitchNavigator(
   {
@@ -56,4 +128,4 @@ const RootNavigator = createSwitchNavigator(
   }
 );
 
-export default createAppContainer(RootNavigator);
+export default App;
